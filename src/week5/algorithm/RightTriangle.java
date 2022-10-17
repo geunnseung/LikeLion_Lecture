595799776cd701d4c21c48ff974dd404211af6dd
seunggeun <bsg1017@naver.com>
2022-10-17 09:25:36 +0900

    별찍기 update -반복문 -메소드

--- src/week5/algorithm/RightTriangle.java
+++ src/week5/algorithm/RightTriangle.java
@@ -1,12 +1,25 @@
 package week5.algorithm;
 
 public class RightTriangle {
+
+    public void printStar(int n) {
+
+        for(int i=1; i <= n; i++){
+            for(int j=0; j<i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
 
         System.out.println("*");
         System.out.println("**");
         System.out.println("***");
         System.out.println("****");
+
+        RightTriangle triangle = new RightTriangle();
+        triangle.printStar(4);
 
     }
 }
